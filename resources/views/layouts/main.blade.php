9e1487b73c433883463d1e7a011afff3965d0543
Yudi Prayoga Raharja <yudiprayogaraharja@gmail.com>
2021-02-17 13:51:25 +0700

    init project

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewpport" content"width-device-width, initial-scale=1">
+    <title>Movie Web</title>
+
+    <link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <nav>Navigation</nav>
+    @yield('content')
+</body>
+</html>
